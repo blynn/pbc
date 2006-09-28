a543fe05c82e3c199af70a60e5fc7c6e8684d57e
Ben Lynn <blynn@cs.stanford.edu>
2006-09-28 13:29:01 -0700

    pbc-0.3.7

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,11 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
-//#define UNUSED_VAR(a) do { /* nothing */ } while (&a == 0)
-#define UNUSED_VAR(a) (void) a
+// from the ACE project: http://www.cs.wustl.edu/~schmidt/ACE.html
+// silences warnings, and generates no code for many compilers
+#define UNUSED_VAR(a) do { /* nothing */ } while (&a == 0)
+
+// I was using this before (and without macros):
+//#define UNUSED_VAR(a) (void) a
 
 #endif
