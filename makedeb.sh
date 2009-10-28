b6e7f02c90767d63d6da06eaa6cea6a699f99a5c
Paul Miller <jettero@cpan.org>
2009-10-28 07:05:36 -0400

    use the new configure.ac rules

--- makedeb.sh
+++ makedeb.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-if [ ! -f pbc/parser.tab.c -o pbc/parser.y -nt pbc/parser.tab.c ]; then
-    bison -d -b pbc/parser pbc/parser.y
-fi
-
-if [ ! -f pbc/lex.yy.c -o pbc/parser.lex -nt pbc/lex.yy.c ]; then
-    flex -o pbc/lex.yy.c --header-file=pbc/lex.yy.h pbc/parser.lex
-fi
-
 dpkg-buildpackage -rfakeroot
