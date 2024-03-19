1b5d226de4788bdcd1d47781c746c4192de5b69c
Ben Lynn <benlynn@gmail.com>
2024-03-18 22:07:16 -0700

    paterson.c: replace element_to_mpz().
    It seems element_from_hash() is what is needed.
    
    Also changed t3 to be a element of Zr.
    
    The element_to_mpz() function is meant to convert a PBC integer to
    a GMP integer. There are no guarantees on its effects on anything
    besides integers, though it seems for field extensions it returns
    the constant term as I once thought that might be useful.

--- example/paterson.c
+++ example/paterson.c
@@ -10,10 +10,8 @@
 int main(int argc, char **argv) {
   pairing_t pairing;
   double time1, time2;
-  element_t Ppub, s, P, R, k, S, Did, Qid, t1, t2, t4, t5, t6, t7, t8,
+  element_t Ppub, s, P, R, k, S, Did, Qid, t1, t2, t3, t4, t5, t6, t7, t8,
     t9, t10, t11;
-  mpz_t t3;
-  mpz_init(t3);
   pbc_demo_pairing_init(pairing, argc, argv);
   if (!pairing_is_symmetric(pairing)) pbc_die("pairing must be symmetric");
 
@@ -31,6 +29,7 @@ int main(int argc, char **argv) {
   element_init_Zr(s, pairing);
   element_init_Zr(k, pairing);
   element_init_Zr(t1, pairing);
+  element_init_Zr(t3, pairing);
 
   element_init_GT(t6, pairing);
   element_init_GT(t8, pairing);
@@ -56,12 +55,12 @@ int main(int argc, char **argv) {
   element_from_hash(t1, "Message", 7);
   element_mul_zn(t2, P, t1);
   //H3(R)=mpz(R);
-//  int n = element_length_in_bytes(R);
-//  unsigned char *data=malloc(n);
-//  element_to_bytes(data, R);
-//  printf("data = %s\n", data);
-  element_to_mpz(t3, R);
-  element_mul_mpz(t4, Did, t3);
+  int n = element_length_in_bytes(R);
+  unsigned char *data=malloc(n);
+  element_to_bytes(data, R);
+  element_from_hash(t3, data, n);
+  element_pow_zn(t4, Did, t3);
+
   element_add(t5, t4, t2);
   element_invert(k, k);
   element_mul_zn(S, t5, k);
@@ -74,8 +73,7 @@ int main(int argc, char **argv) {
   element_mul_zn(t7, P, t1);
   element_pairing(t6, P, t7);
   element_pairing(t8, Ppub, Qid);
-  element_to_mpz(t3, R);
-  element_pow_mpz(t9, t8, t3);
+  element_pow_zn(t9, t8, t3);
   element_printf("t8 = %B\n", t8);
   element_printf("t9 = %B\n", t9);
   element_mul(t10, t6, t9);
